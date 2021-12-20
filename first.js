e102b38e199ce8bbaaa9d565e9be16da5ab6daab
piotrkowalczyk95 <piotrkowalczyk1312@gmail.com>
2021-12-20 01:38:34 +0100

    added questions, better visuals

--- first.js
+++ first.js
@@ -1,8 +1,22 @@
 function go() {
-    if (document.getElementById("student").value != "" && document.getElementById("teacher").value != "") {
+    console.log("weszło")
+    if (document.getElementById("student").value != "") {
         document.getElementById("questions").style.display = "block"
         document.getElementById("first").style.display = "none"
     } else {
         alert("Przed rozpoczęciem testu, należy podać dane!")
     }
 }
+
+function keyListener(event) {
+    if (event.defaultPrevented) {
+        return;
+    }
+
+    var key = event.key || event.keyCode;
+
+    if (key === 'Enter' || key === 13) {
+        doWhateverYouWantNowThatYourKeyWasHit();
+    }
+}
+document.addEventListener('keyup', keyListener);
